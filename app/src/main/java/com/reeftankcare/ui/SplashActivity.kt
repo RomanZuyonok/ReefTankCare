1847da6cd9495851d46769e4aaeb4790d3e5dc0b
Roman <125905743+RomanZuyonok@users.noreply.github.com>
2023-08-08 13:42:51 -0700

    Added splash activity and layout

---
+++ app/src/main/java/com/reeftankcare/ui/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.reeftankcare.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.reeftankcare.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private const val TIME_FINISH_SPLASH = 2500L
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        setContentView(R.layout.activity_splash)
+        lifecycleScope.launch(Dispatchers.Main) {
+            delay(TIME_FINISH_SPLASH)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            this@SplashActivity.finish()
+        }
+    }
+}
